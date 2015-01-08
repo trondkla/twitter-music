74f08f5bfb9acf975d18afd8ace5b4b96852311b
trond klakken <trond.klakken@bekk.no>
2015-01-08 15:49:26 +0100

    Listen to enviroment ports

--- twit.js
+++ twit.js
@@ -1,10 +1,10 @@
 var Twit = require('twit');
 var _ = require('underscore');
-var socket = require('socket.io')(8000);
+var socket = require('socket.io')(process.env.WEBSOCKET_PORT || 8000);
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + "/public")).listen(80);
+connect().use(serveStatic(__dirname + "/public")).listen(process.env.HTTP_PORT || 8080);
 
 var T = new Twit({
   consumer_key: '7rMLsE4faev5mrvfJ5hYreu8w',
